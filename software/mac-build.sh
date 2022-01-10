c8d81d16a05864a4dc6b02034b0e59a168f849bd
Alan Staniforth <alan@apollonia.org>
2022-01-10 15:33:43 +0000

    PROJECT: basic working non-lipo-based universal build. mac-build.sh needs a lot adding to it to support this fully and scsi2sd-util/Makefile nneeds a lot of tidying. Possibly later add support for both lipo/non-lipo build systems and an option to switch between them.

--- software/mac-build.sh
+++ software/mac-build.sh
@@ -3,6 +3,8 @@
 BUILD_ARCH=""
 NOBUILD=""
 BUILD_UNIV=0
+BUILD_OP="all"
+INSTALL_DIR="/usr/local/bin"
 
 if test $# -ne 0 ; then
 	### Options
@@ -10,46 +12,70 @@ if test $# -ne 0 ; then
 	do
 		### Get the option flag and switch on that basis
 		case "$1" in
-			-*) FLAG="$1" 
-				case "$FLAG" in
-					-h) 
-						### Usage information
-						echo "Usage: ./build.sh -h"
-						echo "       ./build.sh [[-a]|[-o]]"
-						echo ""
-						echo "Options:"
-						echo "    -h        Print this usage summary."
-						echo "    -a <arch> Build for i386|x86_64|arm64 architecture only."
-						echo "              No point in trying ppc - wxWidgets requires"
-						echo "              10.7 and the last SDK to support ppc was 10.6"
-						echo "              Disables universal build."
-						echo "    -n        Build only the native architecture."
-						echo "              Disables universal build."
-						echo ""
-						NOBUILD=1
-						break
-					;;
-					-a)
-						### mac build - build only specified architecture
-						shift
-						echo "mac-build.sh - building for ${1} only. Disabling universal builds..."
-						BUILD_ARCH="${1}"
-						BUILD_UNIV=1
-						break
-					;;
-					-n)
-						### mac build - build only host architecture
-						BUILD_ARCH=`uname -m`
-						echo "mac-build.sh - building for native architecture ${BUILD_ARCH} only. Disabling universal builds..."
-						BUILD_UNIV=1
-						break
-					;;
-					-*) echo "Usage: ${1} not understood!"
-						echo "Use ./build.sh -h for usage information"
-						NOBUILD=1
-						break
-					;;
-				esac
+			-h) 
+				### Usage information
+				echo "Usage: ./build.sh -h"
+				echo "       ./build.sh [[-a]|[-o]] [-i <ALT_INSTALL_DIR] [-o BUILD_OPERATION]"
+				echo ""
+				echo "Options:"
+				echo "    -h        Print this usage summary."
+				echo "    -a <arch> Build for i386|x86_64|arm64 architecture only."
+				echo "              No point in trying ppc - wxWidgets requires"
+				echo "              10.7 and the last SDK to support ppc was 10.6"
+				echo "              Overrides -n if follows on command line."
+				echo "              Disables universal build."
+				echo "    -i        Overide the default (/usr/local/bin) install directory for the"
+				echo "              'install' build operation."
+				echo "    -n        Build only the native architecture."
+				echo "              Overrides -a if follows on command line."
+				echo "              Disables universal build."
+				echo "    -o <op>   Do the specified build script operation. Options are:"
+				echo "                  all -       Default. Build all the scsi2sd tools and package"
+				echo "                              them."
+				echo "                  clean -     remove tool executables and objects from the"
+				echo "                              build tree. Respects the -a option."
+				echo "                  treeclean - remove entire build tree."
+				echo "                  distclean - remove entire build tree and distribution tree."
+				echo "                  install -   install tools from distribution tree to local"
+				echo "                              machine."
+				echo ""
+				NOBUILD=1
+				break
+			;;
+			-a)
+				### mac build - build only specified architecture
+				shift
+				echo "mac-build.sh - building for ${1} only. Disabling universal builds..."
+				BUILD_ARCH="${1}"
+				BUILD_UNIV=1
+				shift
+			;;
+			-i)
+				### mac build - overide default install dir (/usr/loca/bin)
+				shift
+				echo "mac-build.sh - will install to ${1}..."
+				INSTALL_DIR="${1}"
+				shift
+			;;
+			-n)
+				### mac build - build only host architecture
+				BUILD_ARCH=`uname -m`
+				echo "mac-build.sh - building for native architecture ${BUILD_ARCH} only. Disabling universal builds..."
+				BUILD_UNIV=1
+				shift
+			;;
+			-o)
+				### mac build - build script operation 
+				shift
+				echo "mac-build.sh - build operation: '$1'"
+				BUILD_OP="${1}"
+				shift
+			;;
+			-*) echo "Usage: ${1} not understood!"
+				echo "Use ./build.sh -h for usage information"
+				NOBUILD=1
+				break
+			;;
 		esac
 	done
 fi
@@ -86,36 +112,84 @@ else
 	cd - > /dev/null
 	rm -rf "${TMP_DIR}"
 fi
-echo "mac-build.sh - building for: ${BUILD_ARCHS[@]}"
 
-### Start build
+ARCH_FLAGS=""
 for BUILD_ARCH in "${BUILD_ARCHS[@]}"; do
-	echo "mac-build.sh - building ${BUILD_ARCH}"
-	cd scsi2sd-util
-	make ARCH="${BUILD_ARCH}" -C ./
-	BUILD_RES=$?
-	cd ..
-	if [ $BUILD_RES -eq 0 ]; then
-		mkdir -p build/mac-${BUILD_ARCH}
-		cp scsi2sd-util/build/mac-${BUILD_ARCH}/scsi2sd-util build/mac-${BUILD_ARCH} && \
-		cp scsi2sd-util/build/mac-${BUILD_ARCH}/scsi2sd-bulk build/mac-${BUILD_ARCH} && \
-		cp scsi2sd-util/build/mac-${BUILD_ARCH}/scsi2sd-monitor build/mac-${BUILD_ARCH}
-	else
-		exit 1
-	fi
+	ARCH_FLAGS="${ARCH_FLAGS} -arch ${BUILD_ARCH}"
 done
+# Trim leading space
+ARCH_FLAGS=`echo "${ARCH_FLAGS}" | xargs`
+cd scsi2sd-util
+make ARCH="${ARCH_FLAGS}" -C ./
+BUILD_RES=$?
+cd ..
 
-### Create distro disk image
-if [ ${#BUILD_ARCHS[@]} -gt 1 ] ; then
-	echo "mac-build.sh - packaging Mac distro (universal for ${BUILD_ARCHS[@]})..."
+# bale out while testing new build system
+exit 0
+
+if [ "${BUILD_OP}" = 'all' -o "${BUILD_OP}" = 'install' ]; then
+	### Start build
+	echo "mac-build.sh - building for: ${BUILD_ARCHS[@]}"
+	for BUILD_ARCH in "${BUILD_ARCHS[@]}"; do
+		echo "mac-build.sh - building ${BUILD_ARCH}"
+		cd scsi2sd-util
+		make ARCH="${BUILD_ARCH}" -C ./
+		BUILD_RES=$?
+		cd ..
+		if [ $BUILD_RES -eq 0 ]; then
+			mkdir -p build/mac-${BUILD_ARCH}
+			cp scsi2sd-util/build/mac-${BUILD_ARCH}/scsi2sd-util build/mac-${BUILD_ARCH} && \
+			cp scsi2sd-util/build/mac-${BUILD_ARCH}/scsi2sd-bulk build/mac-${BUILD_ARCH} && \
+			cp scsi2sd-util/build/mac-${BUILD_ARCH}/scsi2sd-monitor build/mac-${BUILD_ARCH}
+		else
+			exit 1
+		fi
+	done
+
+	### Create distro disk image
+	if [ ${#BUILD_ARCHS[@]} -gt 1 ] ; then
+		echo "mac-build.sh - packaging Mac distro (universal for ${BUILD_ARCHS[@]})..."
+	else
+		echo "mac-build.sh - packaging Mac distro (for ${BUILD_ARCHS[@]})..."
+	fi
+	rm -rf ./build/mac/scsi2sd-util*
+	mkdir -p ./build/mac/scsi2sd-util
+	lipo -create ./build/mac-*/scsi2sd-bulk -output ./build/mac/scsi2sd-util/scsi2sd-bulk
+	lipo -create ./build/mac-*/scsi2sd-monitor -output ./build/mac/scsi2sd-util/scsi2sd-monitor
+	lipo -create ./build/mac-*/scsi2sd-util -output ./build/mac/scsi2sd-util/scsi2sd-util
+	chmod a+rx ./build/mac/scsi2sd-util/*
+	hdiutil create -ov -fs HFS+ -srcfolder ./build/mac/scsi2sd-util ./build/mac/scsi2sd-util.dmg
+	rm -rf ./build/mac-*
+	
+	### install if required
+	if [ "${BUILD_OP}" = 'install' ]; then
+		install -dv "${INSTALL_DIR}"
+		install -v build/mac/scsi2sd-util/scsi2sd-util "${INSTALL_DIR}"
+		install -v build/mac/scsi2sd-util/scsi2sd-monitor "${INSTALL_DIR}"
+		install -v build/mac/scsi2sd-util/scsi2sd-bulk "${INSTALL_DIR}"
+	fi
+elif [ "${BUILD_OP}" = 'clean' ]; then
+	### Clean executables
+	echo "mac-build.sh - cleaning executables and objects for: ${BUILD_ARCHS[@]}"
+	for BUILD_ARCH in "${BUILD_ARCHS[@]}"; do
+		echo "mac-build.sh - cleaning ${BUILD_ARCH} executables and objects..."
+		cd scsi2sd-util
+		make ARCH="${BUILD_ARCH}" -C ./ clean
+		cd ..
+	done
+elif [ "${BUILD_OP}" = 'treeclean' -o "${BUILD_OP}" = 'distclean' ]; then
+	### Remove build tree
+	echo "mac-build.sh - removing entire build tree..."
+	for BUILD_ARCH in "${BUILD_ARCHS[@]}"; do
+		echo "mac-build.sh - deleting ${BUILD_ARCH} build tree..."
+		rm -rf ./scsi2sd-util/build/mac-"${BUILD_ARCH}"
+	done
+	if [ "${BUILD_OP}" = 'distclean' ]; then
+		### Remove distribution tree
+		echo "mac-build.sh - removing distribution tree..."
+		rm -rf ./build
+	fi
 else
-	echo "mac-build.sh - packaging Mac distro (for ${BUILD_ARCHS[@]})..."
+	echo "mac-build.sh - unrecognised build option '${BUILD_OP}'. Exiting..."
+	exit 1
 fi
-rm -rf ./build/mac/scsi2sd-util*
-mkdir -p ./build/mac/scsi2sd-util
-lipo -create ./build/mac-*/scsi2sd-bulk -output ./build/mac/scsi2sd-util/scsi2sd-bulk
-lipo -create ./build/mac-*/scsi2sd-monitor -output ./build/mac/scsi2sd-util/scsi2sd-monitor
-lipo -create ./build/mac-*/scsi2sd-util -output ./build/mac/scsi2sd-util/scsi2sd-util
-chmod a+rx ./build/mac/scsi2sd-util/*
-hdiutil create -ov -fs HFS+ -srcfolder ./build/mac/scsi2sd-util ./build/mac/scsi2sd-util.dmg
-rm -rf ./build/mac/scsi2sd-util ./build/mac-*
